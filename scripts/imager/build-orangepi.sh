3ad07c91037e0f69a5c8734313563e69ab2b0c69
leo <leo@autonomous.ai>
2026-07-24 15:47:08 +0700

    - add intern-v2 variants

--- scripts/imager/build-orangepi.sh
+++ scripts/imager/build-orangepi.sh
@@ -76,6 +76,9 @@ case "${DEVICE_TYPE}" in
   intern-v2)
     DEVICE_BASE_IMG="${DEVICE_BASE_IMG:-/input/intern-v2/golden-opi-dev.img.xz}"
     ;;
+  intern-v2-hermes)
+      DEVICE_BASE_IMG="${DEVICE_BASE_IMG:-/input/intern-v2-hermes/golden-opi-dev.img.xz}"
+      ;;
   *)
     DEVICE_BASE_IMG=""
     ;;
